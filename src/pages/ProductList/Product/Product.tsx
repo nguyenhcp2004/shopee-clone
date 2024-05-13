520d89ba4c920365eb245197f84daffe23e11e73
Huynh Nguyen <yugivip277@gmail.com>
2024-05-13 19:58:40 +0700

    Code UI Product Component

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,3 +1,73 @@
+import { Link } from 'react-router-dom'
+
 export default function Product() {
-  return <div>Product</div>
+  return (
+    <Link to='/'>
+      <div className='bg-white shadow rounded-sm hover:translate-y-[-0.04rem] hover:shadow-md duration-100 transition-transform overflow-hidden'>
+        <div className='w-full pt-[100%] relative'>
+          <img
+            src='https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lg0cvov1k1gq4f_tn'
+            alt=''
+            className='absolute top-0 left-0 bg-white w-full h-full object-cover'
+          />
+        </div>
+        <div className='p-2 overflow-hidden'>
+          <div className='min-h-[2rem] line-clamp-2 text-xs'>
+            [HOT] Loa Bluetooth Mini Loa bluetooth mini di động cầm tay 💥Đèn LED Đổi Màu💥- Tặng Kèm Dây Sạc,Công nghệ
+            blutooth 5.0
+          </div>
+          <div className='flex items-center mt-3'>
+            <div className='line-through max-w-[50%] text-gray truncate'>
+              <span className='text-xs'>₫</span>
+              <span>15.000</span>
+            </div>
+            <div className='ml-1 text-orange truncate'>
+              <span className='text-xs'>₫</span>
+              <span>7.900</span>
+            </div>
+          </div>
+          <div className='mt-3 flex items-center justify-end'>
+            <div className='flex items-center'>
+              <div className='relative'>
+                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '50%' }}>
+                  <svg
+                    enableBackground='new 0 0 15 15'
+                    viewBox='0 0 15 15'
+                    x={0}
+                    y={0}
+                    className='w-3 h-3 fill-yellow-300 text-yellow-300'
+                  >
+                    <polygon
+                      points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeMiterlimit={10}
+                    />
+                  </svg>
+                </div>
+                <svg
+                  enableBackground='new 0 0 15 15'
+                  viewBox='0 0 15 15'
+                  x={0}
+                  y={0}
+                  className='w-3 h-3 fill-current text-gray-300'
+                >
+                  <polygon
+                    points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeMiterlimit={10}
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className='ml-2 text-sm'>
+              <span>10,5k</span>
+              <span className='ml-1'>Đã bán</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
 }
