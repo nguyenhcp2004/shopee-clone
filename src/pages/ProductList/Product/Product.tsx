0e973625c89cf7bb3a385b43f64c41858d006868
Huynh Nguyen <yugivip277@gmail.com>
2024-05-15 14:12:54 +0700

    Code logic phân trang

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,68 +1,39 @@
 import { Link } from 'react-router-dom'
+import ProductRating from '~/components/ProductRating'
+import { Product as ProductType } from '~/types/product.type'
+import { formatCurrency, formatNumberToSocialStyle } from '~/utils/utils'
 
-export default function Product() {
+interface Props {
+  product: ProductType
+}
+
+export default function Product({ product }: Props) {
   return (
     <Link to='/'>
       <div className='bg-white shadow rounded-sm hover:translate-y-[-0.04rem] hover:shadow-md duration-100 transition-transform overflow-hidden'>
         <div className='w-full pt-[100%] relative'>
           <img
-            src='https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lg0cvov1k1gq4f_tn'
-            alt=''
+            src={product.image}
+            alt={product.name}
             className='absolute top-0 left-0 bg-white w-full h-full object-cover'
           />
         </div>
         <div className='p-2 overflow-hidden'>
-          <div className='min-h-[2rem] line-clamp-2 text-xs'>
-            [HOT] Loa Bluetooth Mini Loa bluetooth mini di động cầm tay 💥Đèn LED Đổi Màu💥- Tặng Kèm Dây Sạc,Công nghệ
-            blutooth 5.0
-          </div>
+          <div className='min-h-[2rem] line-clamp-2 text-xs'>{product.name}</div>
           <div className='flex items-center mt-3'>
             <div className='line-through max-w-[50%] text-gray truncate'>
               <span className='text-xs'>₫</span>
-              <span>15.000</span>
+              <span>{formatCurrency(product.price_before_discount)}</span>
             </div>
             <div className='ml-1 text-orange truncate'>
               <span className='text-xs'>₫</span>
-              <span>7.900</span>
+              <span>{formatCurrency(product.price)}</span>
             </div>
           </div>
           <div className='mt-3 flex items-center justify-end'>
-            <div className='flex items-center'>
-              <div className='relative'>
-                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '50%' }}>
-                  <svg
-                    enableBackground='new 0 0 15 15'
-                    viewBox='0 0 15 15'
-                    x={0}
-                    y={0}
-                    className='w-3 h-3 fill-yellow-300 text-yellow-300'
-                  >
-                    <polygon
-                      points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeMiterlimit={10}
-                    />
-                  </svg>
-                </div>
-                <svg
-                  enableBackground='new 0 0 15 15'
-                  viewBox='0 0 15 15'
-                  x={0}
-                  y={0}
-                  className='w-3 h-3 fill-current text-gray-300'
-                >
-                  <polygon
-                    points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeMiterlimit={10}
-                  />
-                </svg>
-              </div>
-            </div>
+            <ProductRating rating={product.rating} />
             <div className='ml-2 text-sm'>
-              <span>10,5k</span>
+              <span>{formatNumberToSocialStyle(product.sold)}</span>
               <span className='ml-1'>Đã bán</span>
             </div>
           </div>
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -4,16 +4,19 @@ import Product from './Product/Product'
 import SortProductList from './SortProductList'
 import useQueryParams from '~/hooks/useQueryParams'
 import productApi from '~/apis/product.api'
+import Pagination from '~/components/Pagination'
+import { useState } from 'react'
 
 export default function ProductList() {
   const queryParams = useQueryParams()
+  const [page, setPage] = useState(1)
   const { data } = useQuery({
     queryKey: ['products', queryParams],
     queryFn: () => {
       return productApi.getProducts(queryParams)
     }
   })
-  console.log(data)
+
   return (
     <div className='bg-gray-200 py-6'>
       <div className='container'>
@@ -24,14 +27,14 @@ export default function ProductList() {
           <div className='col-span-9'>
             <SortProductList />
             <div className='mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3'>
-              {Array(30)
-                .fill(0)
-                .map((_, index) => (
-                  <div className='col-span-1' key={index}>
-                    <Product />
+              {data &&
+                data.data.data.products.map((product) => (
+                  <div className='col-span-1' key={product._id}>
+                    <Product product={product} />
                   </div>
                 ))}
             </div>
+            <Pagination page={page} setPage={setPage} pageSize={4} />
           </div>
         </div>
       </div>
