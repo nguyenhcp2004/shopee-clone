d2298f2bc078cec3eddbff270958fa683be11ac5
Huynh Nguyen <yugivip277@gmail.com>
2024-05-27 16:39:12 +0700

    Phân tích và khai báo API purchases

---
+++ src/apis/purchase.api.ts
@@ -0,0 +1,19 @@
+import { Purchase, PurchaseListStatus } from '~/types/purchase.type'
+import { SucccessResponse } from '~/types/utils.type'
+import http from '~/utils/http'
+
+const URL = 'purchases'
+
+const purchaseApi = {
+  addToCart(body: { product_id: string; buyCount: number }) {
+    return http.post<SucccessResponse<Purchase>>(`${URL}/add-to-cart`, body)
+  },
+
+  getPurchases(params: { status: PurchaseListStatus }) {
+    return http.get<SucccessResponse<Purchase[]>>(`${URL}`, {
+      params
+    })
+  }
+}
+
+export default purchaseApi
