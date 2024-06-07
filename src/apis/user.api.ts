91a3dafaa209072ab2675d308565f39a6c2c6b87
Huynh Nguyen <yugivip277@gmail.com>
2024-06-07 22:19:15 +0700

    Khai báo và test api cho phần proflie

---
+++ src/apis/user.api.ts
@@ -0,0 +1,27 @@
+import { User } from '~/types/user.type'
+import { SucccessResponse } from '~/types/utils.type'
+import http from '~/utils/http'
+
+interface BodyUpdateProfile extends Omit<User, '_id' | 'roles' | 'createdAt' | 'updatedAt' | 'email'> {
+  password?: string
+  newPassword?: string
+}
+
+const userApi = {
+  getProfile() {
+    return http.get<SucccessResponse<User>>('me')
+  },
+
+  updateProfile(body: BodyUpdateProfile) {
+    return http.put('user', body)
+  },
+
+  uploadAvatar(body: FormData) {
+    return http.post('user/upload-avatar', body, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+  }
+}
+export default userApi
